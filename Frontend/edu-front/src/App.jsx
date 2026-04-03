ac8985ba26d51c0aca42bc30615812511bd5c1df
Abiy <atilahun144@gmail.com>
2026-04-03 20:48:52 +0300

    Add AIInterface

--- Frontend/edu-front/src/App.jsx
+++ Frontend/edu-front/src/App.jsx
@@ -1,19 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Componets/Navbar";
- import Hero from "./Componets/Hero";
+import Hero from "./Componets/Hero";
 import ToolsSection from "./Componets/ToolsSection";
 import StatsSection from "./Componets/StatsSection";
 import UserRatings from "./Componets/UserRatings";
 import Footer from "./Componets/Footer";
+import AIInterface from "./Componets/AIInterface";
+
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Hero /> 
-      <ToolsSection /> 
-      <StatsSection />
-      <UserRatings/>
-      <Footer/>
-    </div>
+    <Router>
+      <Navbar /> {/* Navbar stays fixed for all pages */}
+      <Routes>
+        {/* Home Page */}
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <ToolsSection />
+              <StatsSection />
+              <UserRatings />
+              <Footer />
+            </>
+          }
+        />
+
+        {/* AI Interface Page */}
+        <Route
+          path="/ai"
+          element={
+            <>
+              <AIInterface />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
